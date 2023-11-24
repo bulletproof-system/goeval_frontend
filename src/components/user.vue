eea1fed4ef82fc9fc58db5cc247c875da4b57730
LTT <sd_ltt@qq.com>
2023-11-24 15:48:06 +0800

    阻止 form 默认提交功能

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,108 +1,110 @@
 <template>
-	<div class="card-warp">
-		<el-card class="box-card">
-			<el-row>
-				<el-col :span="16">
-					<el-descriptions
-						:column="1">
-						<el-descriptions-item>
-							<template #label>
-								<el-text>
-									<el-icon><User /></el-icon>
-									{{ t('userInfo.userName') }}
-								</el-text>
-							</template>
-							<el-text>
-								{{ userInfo.username }}
-							</el-text>
-						</el-descriptions-item>
-						<el-descriptions-item>
-							<template #label>
-								<el-text>
-									<el-icon><Message /></el-icon>
-									{{ t('userInfo.email') }}
-								</el-text>
-							</template>
-							<el-text>
-								{{ userInfo.email }}
-							</el-text>
-						</el-descriptions-item>
-						<el-descriptions-item>
-							<template #label>
-								<el-text>
-										<el-icon><Key /></el-icon>
-										{{ t('userInfo.role.name') }}
+	<div>
+		<div class="card-warp">
+			<el-card class="box-card">
+				<el-row>
+					<el-col :span="16">
+						<el-descriptions
+							:column="1">
+							<el-descriptions-item>
+								<template #label>
+									<el-text>
+										<el-icon><User /></el-icon>
+										{{ t('userInfo.userName') }}
 									</el-text>
 								</template>
 								<el-text>
-									{{ t(`userInfo.role.${userInfo.role}`) }}
+									{{ userInfo.username }}
 								</el-text>
-						</el-descriptions-item>
-					</el-descriptions>
-				</el-col>
-				<el-col :span="8">
-					<el-avatar :size="100" :src="userInfo.avatar" />
-				</el-col>
-			</el-row>
-			<el-divider />
-			<div>
-				<el-button type="primary" text bg @click="operateEmail = true">
-					{{ t('userInfo.operate.email.detail') }}
-				</el-button>
-				<el-button type="primary" text bg @click="operateAvatar">
-					{{ t('userInfo.operate.avatar') }}
-				</el-button>
-				<el-button type="danger" text bg @click="operatePassword = true">
-					{{ t('userInfo.operate.password.detail') }}
-				</el-button>
-				<el-button type="danger" text bg @click="userInfo.logout">
-					{{ t('userInfo.operate.logout') }}
-				</el-button>
-			</div>
-		</el-card>
+							</el-descriptions-item>
+							<el-descriptions-item>
+								<template #label>
+									<el-text>
+										<el-icon><Message /></el-icon>
+										{{ t('userInfo.email') }}
+									</el-text>
+								</template>
+								<el-text>
+									{{ userInfo.email }}
+								</el-text>
+							</el-descriptions-item>
+							<el-descriptions-item>
+								<template #label>
+									<el-text>
+											<el-icon><Key /></el-icon>
+											{{ t('userInfo.role.name') }}
+										</el-text>
+									</template>
+									<el-text>
+										{{ t(`userInfo.role.${userInfo.role}`) }}
+									</el-text>
+							</el-descriptions-item>
+						</el-descriptions>
+					</el-col>
+					<el-col :span="8">
+						<el-avatar :size="100" :src="userInfo.avatar" />
+					</el-col>
+				</el-row>
+				<el-divider />
+				<div>
+					<el-button type="primary" text bg @click="operateEmail = true">
+						{{ t('userInfo.operate.email.detail') }}
+					</el-button>
+					<el-button type="primary" text bg @click="operateAvatar">
+						{{ t('userInfo.operate.avatar') }}
+					</el-button>
+					<el-button type="danger" text bg @click="operatePassword = true">
+						{{ t('userInfo.operate.password.detail') }}
+					</el-button>
+					<el-button type="danger" text bg @click="userInfo.logout">
+						{{ t('userInfo.operate.logout') }}
+					</el-button>
+				</div>
+			</el-card>
+		</div>
+		<el-dialog
+			v-model="operateEmail"
+			:title="t('userInfo.operate.email.detail')">
+			<el-form :model="emailForm" ref="emailFormRef" :rules="emailRules" status-icon @submit.native.prevent>
+				<el-form-item :label="t('userInfo.operate.email.new')" :error="emailError" prop="email">
+					<el-input v-model="emailForm.email" autocomplete="off" @keyup.enter="confirmEmail(emailFormRef)"/>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="operateEmail = false" text>
+						{{ t('userInfo.operate.cancel') }}
+					</el-button>
+					<el-button type="primary" @click="confirmEmail(emailFormRef)" text bg>
+						{{ t('userInfo.operate.confirm') }}
+					</el-button>
+				</el-form-item>
+			</el-form>
+		</el-dialog>
+		<el-dialog
+			v-model="operatePassword"
+			:title="t('userInfo.operate.password.detail')">
+			<el-form :model="passwordForm" ref="passwordFormRef" :rules="passwordRules" 
+				status-icon hide-required-asterisk @submit.native.prevent>
+				<el-form-item :label="t('userInfo.operate.password.old')" :error="passwordError.oldPassword" prop="oldPassword">
+					<el-input v-model="passwordForm.oldPassword" autocomplete="off" 
+						clearable show-password
+					/>
+				</el-form-item>
+				<el-form-item :label="t('userInfo.operate.password.new')" :error="passwordError.newPassword" prop="newPassword">
+					<el-input v-model="passwordForm.newPassword" autocomplete="off" 
+						clearable show-password @keyup.enter="confirmPassword(passwordFormRef)"
+					/>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="operatePassword = false" text>
+						{{ t('userInfo.operate.cancel') }}
+					</el-button>
+					<el-button type="primary" @click="confirmPassword(passwordFormRef)" text bg>
+						{{ t('userInfo.operate.confirm') }}
+					</el-button>
+				</el-form-item>
+			</el-form>
+		</el-dialog>
 	</div>
-	<el-dialog
-		v-model="operateEmail"
-		:title="t('userInfo.operate.email.detail')">
-		<el-form :model="emailForm" ref="emailFormRef" :rules="emailRules" status-icon>
-			<el-form-item :label="t('userInfo.operate.email.new')" :error="emailError" prop="email">
-				<el-input v-model="emailForm.email" autocomplete="off" />
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="operateEmail = false" text>
-					{{ t('userInfo.operate.cancel') }}
-				</el-button>
-				<el-button type="primary" @click="confirmEmail(emailFormRef)" text bg>
-					{{ t('userInfo.operate.confirm') }}
-				</el-button>
-			</el-form-item>
-		</el-form>
-	</el-dialog>
-	<el-dialog
-		v-model="operatePassword"
-		:title="t('userInfo.operate.password.detail')">
-		<el-form :model="passwordForm" ref="passwordFormRef" :rules="passwordRules" 
-			status-icon hide-required-asterisk>
-			<el-form-item :label="t('userInfo.operate.password.old')" :error="passwordError.oldPassword" prop="oldPassword">
-				<el-input v-model="passwordForm.oldPassword" autocomplete="off" 
-					clearable show-password
-				/>
-			</el-form-item>
-			<el-form-item :label="t('userInfo.operate.password.new')" :error="passwordError.newPassword" prop="newPassword">
-				<el-input v-model="passwordForm.newPassword" autocomplete="off" 
-					clearable show-password
-				/>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="operatePassword = false" text>
-					{{ t('userInfo.operate.cancel') }}
-				</el-button>
-				<el-button type="primary" @click="confirmPassword(passwordFormRef)" text bg>
-					{{ t('userInfo.operate.confirm') }}
-				</el-button>
-			</el-form-item>
-		</el-form>
-	</el-dialog>
 </template>
 
 <script setup lang="ts">
