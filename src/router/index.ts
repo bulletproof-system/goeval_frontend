566a0861a5eb7cbec600af7c12dc809d59547004
LTT <sd_ltt@qq.com>
2023-11-27 15:36:36 +0800

    登录框检查 role

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 // import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory  } from 'vue-router';
+import { UserRole } from '@/types/user';
 
 const routes = [
 	{ path: '/', redirect: '/home' },
@@ -7,16 +8,25 @@ const routes = [
 		path: '/home', 
 		component: () => import('@components/home.vue'),
 		name: 'home',
+		meta: {
+			permission: [UserRole.Visitor, UserRole.User, UserRole.Administrator]
+		}
 	}, 
 	{
 		path: '/user',
 		component: () => import('@components/user.vue'),
 		name: 'user',
+		meta: {
+			permission: [UserRole.User, UserRole.Administrator]
+		}
 	},
 	{
 		path: '/star',
 		component: () => import('@components/star.vue'),
 		name: 'star',
+		meta: {
+			permission: [UserRole.User, UserRole.Administrator]
+		}
 		// beforeEnter: [checkLoginState]
 	},
 	{
@@ -30,14 +40,23 @@ const routes = [
 			{
 				path: 'user',
 				component: () => import('@/components/manage/user.vue'),
+				meta: {
+					permission: [UserRole.Administrator]
+				}
 			},
 			{
 				path: 'course',
 				component: () => import('@/components/manage/course.vue'),
+				meta: {
+					permission: [UserRole.Administrator]
+				}
 			},
 			{
 				path: 'announcement',
 				component: () => import('@/components/manage/announcement.vue'),
+				meta: {
+					permission: [UserRole.Administrator]
+				}
 			}
 		]
 	},
@@ -45,11 +64,17 @@ const routes = [
 		path: '/course/:course_id/:review_id/:comment_id?',
 		component: () => import('@components/courseDetail.vue'),
 		name: 'course',
+		meta: {
+			permission: [UserRole.Visitor, UserRole.User, UserRole.Administrator]
+		}
 	},
 	{
 		path:'/home/detail',
 		component: defineAsyncComponent(() => import('@components/detailPage.vue')),
 		name: 'detail',
+		meta: {
+			permission: [UserRole.Visitor, UserRole.User, UserRole.Administrator]
+		}
 	}
 ];
 
